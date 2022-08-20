8ba210abce54aadb6be17943fb22206e5be2c14e
Sunghyun Cho <mail@chosunghyun.com>
2022-08-19 17:26:15 -0700

    add sa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import '../font/tossface.css'
+import Script from 'next/script'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,6 +12,15 @@ const App = ({ Component, pageProps }: AppProps) => {
         crossOrigin='anonymous'
         href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/variable/pretendardvariable.css'
       />
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src='https://queue.simpleanalyticscdn.com/noscript.gif'
+          alt=''
+          referrerPolicy='no-referrer-when-downgrade'
+        />
+      </noscript>
       <div className='min-h-screen px-6 py-8 mx-auto bg-white shadow-xl dark:bg-slate-800 ring-1 ring-slate-900/5'>
         <div className='max-w-screen-xl px-4 mx-auto sm:px-6 md:px-8'>
           <Component {...pageProps} />
