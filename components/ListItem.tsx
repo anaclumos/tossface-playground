870fd66eb83e840973f8b42e018024a7a643e586
Sunghyun Cho <mail@chosunghyun.com>
2022-08-21 22:27:57 -0700

    auto-commit.

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -12,7 +12,9 @@ const ListItem = ({ title, url, text }: Props) => {
       target='_blank'
       rel='noopener noreferrer'
     >
-      <div className='items-center h-full p-2 py-2 mx-auto transition-colors rounded-lg hover:bg-slate-800 bg-slate-100 dark:bg-slate-700 active:bg-slate-200 dark:active:bg-slate-800'>
+      <div
+        className='items-center h-full p-2 py-2 mx-auto transition-colors rounded-lg active:bg-slate-200 bg-slate-100 dark:bg-slate-700 dark:active:bg-slate-800'
+      >
         <h3>{title}</h3>
         <div className='mt-2 text-sm text-slate-500 dark:text-slate-400'>{text}</div>
       </div>
