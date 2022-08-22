3f761d32ea4130e79e0e5f35ba37dd3e8528b01a
Sunghyun Cho <mail@chosunghyun.com>
2022-08-21 21:07:22 -0700

    modify header spacings

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <nav className='py-4 bg-white rounded-lg md:flex md:items-center md:justify-between dark:bg-slate-800'>
-      <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+    <nav className='py-4 mx-auto text-center md:flex md:items-center md:justify-between dark:bg-slate-800 md:text-left lg:text-left'>
+      <span className='mx-auto text-sm text-center text-gray-500 dark:text-gray-400 sm:mx-0'>
         <a href='https://cho.sh/' className='hover:underline' target='_blank' rel='noopener noreferrer'>
           Sunghyun Cho
         </a>
@@ -17,7 +17,7 @@ const Header = () => {
           Open Source
         </a>
       </span>
-      <ul className='grid items-center grid-cols-3 py-2 mt-3 text-sm text-right text-gray-500 dark:text-gray-400'>
+      <ul className='grid items-center grid-cols-3 py-2 mx-auto mt-3 text-sm text-center text-gray-500 lg:text-right dark:text-gray-400 sm:mx-0'>
         <li className='hover:underline'>
           <a href='https://toss.im/tossface'>Tossface</a>
         </li>
