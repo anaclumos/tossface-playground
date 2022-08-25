cb95aa85d032f11a34d51c64525f7cd9faa2d5ff
Sunghyun Cho <mail@chosunghyun.com>
2022-08-25 00:05:18 -0700

    Remove Gaps in Mobile /more

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -7,14 +7,12 @@ type Props = {
 const ListItem = ({ title, url, text }: Props) => {
   return (
     <a
-      className='mt-5 text-base font-medium text-slate-900 dark:text-white'
+      className='text-base font-medium text-slate-900 dark:text-white'
       href={url}
       target='_blank'
       rel='noopener noreferrer'
     >
-      <div
-        className='items-center h-full mx-auto transition-colors rounded-lg active:bg-slate-200 bg-slate-100 dark:bg-slate-700 dark:active:bg-slate-800'
-      >
+      <div className='items-center h-full p-2 mx-auto transition-colors rounded-lg active:bg-slate-200 bg-slate-100 dark:bg-slate-700 dark:active:bg-slate-800'>
         <h3>{title}</h3>
         <div className='mt-2 text-sm text-slate-500 dark:text-slate-400'>{text}</div>
       </div>
